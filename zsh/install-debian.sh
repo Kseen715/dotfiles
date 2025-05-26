0dcc92c200391444e911f83a7e8f6206272127c6
Kseen715 <user@localhost.localdomain>
2025-05-26 20:33:46 +0300

    Added autoinstaller for zsh + addons

--- zsh/install-debian.sh
+++ zsh/install-debian.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# Gather modules
 sudo apt update \
 && sudo apt install curl zsh \
-&& (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh) \
-&& echo "Rust installed. Opening new shell..." \
-&& exec zsh -c ". \$HOME/.cargo/env source && cargo install starship --locked && ./install.sh"
+&& sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Check if cargo is available
+if ! command -v cargo &> /dev/null
+then
+    echo "Cargo not found, installing Rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source "$HOME/.cargo/env"
+    echo "Rust installed."
+else
+    echo "Cargo already installed, skipping Rust installation."
+fi
+
+# Install starship and run install script
+cargo install starship --locked
+
+# Update configs
+./install.sh -y \
+&& sudo chsh -s $(which zsh)
