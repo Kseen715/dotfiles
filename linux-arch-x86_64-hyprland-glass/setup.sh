30c7a81dd05b42cd6b527136ea9ae8c05657140f
kseen <korenevdn@gmail.com>
2025-07-01 02:53:54 +0300

    Refactor setup script and update clipboard management binding

--- linux-arch-x86_64-hyprland-glass/setup.sh
+++ linux-arch-x86_64-hyprland-glass/setup.sh
@@ -1,98 +1,28 @@
 #!/bin/bash
-# ==============================================================================
 
-# Grab --delevated <username> argument if provided
-if [[ "$1" == "--delevated" && -n "$2" ]]; then
-    DELEVATED_USER="$2"
-    shift 2 # Remove the first two arguments
-else
-    DELEVATED_USER=""
-fi
-
-# Signal handler for Ctrl+C
-cleanup() {
-    echo ""
-    error "Script interrupted by user (Ctrl+C). Exiting..."
-}
-
-# Trap SIGINT (Ctrl+C) and call cleanup function
-trap cleanup SIGINT SIGTERM SIGQUIT
-
-# Color codes
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-CYAN='\033[0;36m'
-NC='\033[0m' # No Color
-
-# Enhanced logging functions with colors
-echo() {
-    printf "${CYAN}[INFO]${NC}\t%s\n" "$*"
-}
-
-warning() {
-    printf "${YELLOW}[WARN]${NC}\t%s\n" "$*" >&2
-}
-
-error() {
-    printf "${RED}[ERROR]${NC}\t%s\n" "$*" >&2
-    exit 1
-}
-
-success() {
-    printf "${GREEN}[DONE]${NC}\t%s\n" "$*"
-}
-
-trace() {
-    printf "${NC}[BASH]${NC}\t%s\n" "$*"
-    "$@"
-}
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-echo "Checking if root..."
-if [[ $EUID -ne 0 ]]; then
-    # If not root, save username and re-execute with sudo
-    USERNAME=$(whoami)
-    echo "Current user: $USERNAME"
-    if ! command -v sudo &>/dev/null; then
-        error "This script must be run as root. Use 'su' to switch to root user or install sudo"
-    else
-        warning "Running with sudo..."
-        # use absolute path to the script to avoid issues with relative paths
-        if [[ ! -f "$SCRIPT_DIR/$(basename "$0")" ]]; then
-            error "Script not found at expected location: $SCRIPT_DIR/$(basename "$0")"
-        fi
-        # Re-executes the script with sudo
-        trace chmod +x "$SCRIPT_DIR/$(basename "$0")"
-        exec sudo "$SCRIPT_DIR/$(basename "$0")" --delevated "$USERNAME" "$@"
-        exit 0
-    fi
-fi
-
-# ==============================================================================
+source "$(dirname "$(realpath "$0")")/common.sh"
 
 TMP_FOLDER="/tmp/setup"
 trace mkdir -p $TMP_FOLDER
 
 # Update sources
-echo "Updating package sources..."
+info "Updating package sources..."
 trace pacman -Sy --noconfirm 
 
 # Ensure git is installed
 if ! command -v git &>/dev/null; then
-    echo "Git not found. Installing git..."
+    info "Git not found. Installing git..."
     trace pacman -S --needed --noconfirm git
 fi
 
 # Check if yay and/or paru is installed and chose one as preferred AUR helper.
 # (paru is preferred if both are installed)
 if command -v yay &>/dev/null; then
-    echo "YAY detected"
+    info "YAY detected"
     AUR_HELPER="yay"
 fi
 if command -v paru &>/dev/null; then
-    echo "PARU detected"
+    info "PARU detected"
     AUR_HELPER="paru"
 fi
 if [ -z "$AUR_HELPER" ]; then
@@ -101,39 +31,41 @@ if [ -z "$AUR_HELPER" ]; then
     # run as non-root user to avoid permission issues
     trace bash "$SCRIPT_DIR/build-paru.sh" --delevated "$DELEVATED_USER"
 fi
-echo "Using $AUR_HELPER as the AUR helper"
+info "Using $AUR_HELPER as the AUR helper"
 
 VIRT=""
 # Detect virtualizations (VMware, VirtualBox, QEMU, etc.)
 if command -v lscpu &>/dev/null; then
     if lscpu | grep -q "VMware"; then
-        echo "VMware detected"
+        info "VMware detected"
         VIRT="vmware"
     elif lscpu | grep -q "VirtualBox"; then
-        echo "VirtualBox detected"
+        info "VirtualBox detected"
         VIRT="virtualbox"
     elif lscpu | grep -q "QEMU"; then
-        echo "QEMU detected"
+        info "QEMU detected"
         VIRT="qemu"
     fi
 fi
 
-# Install minimal text editors 
-echo "Installing minimal text editors..."
+info "Installing minimal text editors..."
 trace pacman -S --needed --noconfirm nano vim
 
-echo "Downloading dotfiles from Kseen715..."
+info "Installing wget..."
+trace pacman -S --needed --noconfirm wget
+
+info "Downloading dotfiles from Kseen715..."
 DOTFILES_KSEEN715_REPO="$TMP_FOLDER/dotfiles_Kseen715"
 trace rm -rf $DOTFILES_KSEEN715_REPO
 trace git clone https://github.com/Kseen715/dotfiles $DOTFILES_KSEEN715_REPO --depth 1
 
-echo "Installing video drivers..."
+info "Installing video drivers..."
 # Install video drivers based on virtualization type and hardware if not virtualized
 if [ "$VIRT" = "vmware" ]; then
-    echo "Detected VMware, installing VMware specific GPU drivers..."
+    info "Detected VMware, installing VMware specific GPU drivers..."
     trace pacman -S --needed --noconfirm open-vm-tools mesa
     sudo -u "$DELEVATED_USER" $AUR_HELPER -S --needed --noconfirm xf86-video-vmware-git
-    echo "Activating VMware tools..."
+    info "Activating VMware tools..."
     trace systemctl enable vmtoolsd.service --force
     trace systemctl enable vmware-vmblock-fuse.service --force
 fi
@@ -143,17 +75,17 @@ GPU_VENDOR=""
 if command -v lspci &>/dev/null; then
     GPU_VENDOR=$(lspci | grep -E "VGA|3D" | awk -F: '{print $3}' | awk '{print $1}')
     if [[ "$GPU_VENDOR" == "NVIDIA" ]]; then
-        echo "NVIDIA GPU detected"
+        info "NVIDIA GPU detected"
     elif [[ "$GPU_VENDOR" == "AMD" ]]; then
-        echo "AMD GPU detected"
+        info "AMD GPU detected"
     elif [[ "$GPU_VENDOR" == "Intel" ]]; then
-        echo "Intel GPU detected"
+        info "Intel GPU detected"
     elif [[ "$GPU_VENDOR" == "VMware" ]]; then
-        echo "VMware GPU detected"
+        info "VMware GPU detected"
     elif [[ "$GPU_VENDOR" == "VirtualBox" ]]; then
-        echo "VirtualBox GPU detected"
+        info "VirtualBox GPU detected"
     elif [[ -z "$GPU_VENDOR" ]]; then
-        echo "No GPU detected, assuming virtualized environment with no dedicated GPU"
+        info "No GPU detected, assuming virtualized environment with no dedicated GPU"
     else
         warning "Unknown GPU vendor: $GPU_VENDOR"
     fi
@@ -163,47 +95,47 @@ fi
 
 # add multilib repository to pacman.conf if not already present (can be commented out, if so - add it anyway)
 if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
-    echo "Adding multilib repository to /etc/pacman.conf"
+    info "Adding multilib repository to /etc/pacman.conf"
     trace sudo tee -a /etc/pacman.conf <<EOF
 
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 EOF
 else
-    echo "Multilib repository already exists in /etc/pacman.conf"
+    info "Multilib repository already exists in /etc/pacman.conf"
 fi
 trace pacman -Sy
 
-echo "Installing GPU drivers..."
+info "Installing GPU drivers..."
 if [ "$GPU_VENDOR" == "NVIDIA" ]; then
-    echo "NVIDIA GPU detected"
+    info "NVIDIA GPU detected"
     trace sudo pacman -S --needed --noconfirm nvidia nvidia-utils lib32-nvidia-utils vulkan-icd-loader lib32-vulkan-icd-loader nvidia-settings
 fi
 if [ "$GPU_VENDOR" == "AMD" ]; then
-    echo "AMD GPU detected"
+    info "AMD GPU detected"
     trace sudo pacman -S --needed --noconfirm mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
 fi
 if [ "$GPU_VENDOR" == "Intel" ]; then
-    echo "Intel GPU detected"
+    info "Intel GPU detected"
     trace sudo pacman -S --needed --noconfirm mesa lib32-mesa vulkan-intel lib32-vulkan-intel
 fi
 if [ "$GPU_VENDOR" == "VMware" ]; then
-    echo "VMware GPU detected"
+    info "VMware GPU detected"
     trace sudo pacman -S --needed --noconfirm open-vm-tools mesa lib32-vulkan-virtio
 fi
 
-echo "Installing wayland..."
+info "Installing wayland..."
 trace pacman -S --needed --noconfirm xorg-xwayland xorg-xlsclients qt5-wayland qt6-wayland glfw-wayland gtk3 gtk4 meson wayland libxcb xcb-util-wm xcb-util-keysyms pango cairo libinput libglvnd uwsm  wayland-protocols wayland-utils wl-clipboard xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr xdg-utils wayland-protocols wayland-utils wlr-protocols
-echo "Installing wayland dotfiles..."
+info "Installing wayland dotfiles..."
 trace mkdir -p /usr/share/wayland-sessions
 
-echo "Installing hyprland..."
+info "Installing hyprland..."
 # check if hyprland is not already installed
 if ! command -v hyprctl &>/dev/null; then
     trace rm /usr/share/wayland-sessions/hyprland.desktop
 fi
 trace pacman -S --needed --noconfirm hyprland hyprshot xdg-desktop-portal-hyprland
-echo "Installing hyprland dotfiles..."
+info "Installing hyprland dotfiles..."
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/hypr
 
@@ -216,6 +148,17 @@ trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/hy
 trace cp config/hypr/start-cliphist-store.sh /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
 trace chmod +x /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
 trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
+info "Checking if Golang installed..."
+if ! command -v go &>/dev/null; then
+    info "Golang not found. Installing Golang..."
+    trace pacman -S --needed --noconfirm go
+else
+    info "Golang is installed"
+fi
+trace go install github.com/pdf/cliphist-wofi-img@latest
+trace wget https://raw.githubusercontent.com/sentriz/cliphist/refs/heads/master/contrib/cliphist-wofi-img -O /usr/local/bin/cliphist-wofi-img
+trace chmod +x /usr/local/bin/cliphist-wofi-img
+trace chown "$DELEVATED_USER":"$DELEVATED_USER" /usr/local/bin/cliphist-wofi-img
 
 trace cp config/wayland-sessions/hyprland.desktop /usr/share/wayland-sessions/hyprland.desktop
 if [ "$VIRT" = "vmware" ]; then
@@ -226,68 +169,68 @@ if [ "$VIRT" = "vmware" ]; then
     trace chown "$DELEVATED_USER":"$DELEVATED_USER" /usr/share/wayland-sessions/start-hyprland-vmware.sh
 fi
 
-echo "Installing sddm..."
+info "Installing sddm..."
 trace pacman -S --needed --noconfirm sddm qt6-5compat qt6-declarative qt6-svg
-echo "Installing sddm dotfiles..."
+info "Installing sddm dotfiles..."
 trace mkdir -p /etc/sddm.conf.d
 trace cp config/sddm/hyprland.main.conf /etc/sddm.conf.d/sddm.conf
-echo "Installing sddm theme..." # config/sddm/jakoolit-theme
+info "Installing sddm theme..." # config/sddm/jakoolit-theme
 trace mkdir -p /usr/share/sddm/themes/jakoolit-theme
 trace cp -r config/sddm/jakoolit-theme /usr/share/sddm/themes
 trace mkdir -p /etc/sddm.conf.d
 trace cp config/sddm/theme.conf.user /etc/sddm.conf.d/theme.conf.user
-echo "Activating sddm..."
+info "Activating sddm..."
 trace systemctl enable sddm.service --force 
 
-echo "Installing hyprpaper..."
+info "Installing hyprpaper..."
 trace pacman -S --needed --noconfirm hyprpaper
-echo "Installing hyprpaper dotfiles..."
+info "Installing hyprpaper dotfiles..."
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/hypr
 trace cp config/hypr/hyprpaper.conf /home/$DELEVATED_USER/.config/hypr/
 
-echo "Installing hyprpicker..."
+info "Installing hyprpicker..."
 trace pacman -S --needed --noconfirm hyprpicker
 
-echo "Installing waybar..."
+info "Installing waybar..."
 trace pacman -S --needed --noconfirm waybar gsimplecal
-trace echo "Installing waybar dotfiles..."
+trace info "Installing waybar dotfiles..."
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/waybar
 trace cp config/waybar/config.jsonc /home/$DELEVATED_USER/.config/waybar/
 trace cp config/waybar/style.css /home/$DELEVATED_USER/.config/waybar/
 
-echo "Installing zsh, dependencies and dotfiles..."
+info "Installing zsh, dependencies and dotfiles..."
 trace cd $DOTFILES_KSEEN715_REPO/zsh
 trace chmod +x ./install-run.sh
 sudo -u "$DELEVATED_USER" ./install-run.sh -y
 trace cd $SCRIPT_DIR
 
-echo "Installing helvum, easyeffects..."
+info "Installing helvum, easyeffects..."
 trace pacman -S --needed --noconfirm helvum easyeffects
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/dconf
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/easyeffects/
-echo "Installing easyeffects plugins..."
+info "Installing easyeffects plugins..."
 trace pacman -S --needed --noconfirm lsp-plugins lsp-plugins-ladspa calf libebur128 zam-plugins zita-convolver speex soundtouch rnnoise libsamplerate libsndfile libbs2b fftw speexdsp nlohmann-json onetbb
 sudo -u "$DELEVATED_USER" paru -S --needed --noconfirm mda-lv2-git libdeep_filter_ladspa-bin calf-ladspa
 
-echo "Installing wofi..."
+info "Installing wofi..."
 trace pacman -S --needed --noconfirm wofi
-echo "Installing wofi dotfiles..."
+info "Installing wofi dotfiles..."
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/wofi
 trace cp config/wofi/config /home/$DELEVATED_USER/.config/wofi/
 trace cp config/wofi/style.css /home/$DELEVATED_USER/.config/wofi/
 
-echo "Installing wezterm..."
+info "Installing wezterm..."
 trace pacman -S --needed --noconfirm wezterm
-echo "Installing dotfiles for wezterm..."
+info "Installing dotfiles for wezterm..."
 trace cd $DOTFILES_KSEEN715_REPO/wezterm
 trace pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd noto-fonts-emoji
 trace chmod +x ./install.sh
 trace ./install.sh -y
 trace cd $SCRIPT_DIR
 
-echo "Installing foot..."
+info "Installing foot..."
 trace pacman -S --needed --noconfirm foot
-echo "Installing foot dotfiles..."
+info "Installing foot dotfiles..."
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/foot
 trace cp config/foot/foot.ini /home/$DELEVATED_USER/.config/foot/
 
