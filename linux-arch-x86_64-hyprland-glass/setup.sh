074ae627e82d0115318af4680715ef1455bf17f7
kseen <korenevdn@gmail.com>
2025-07-04 01:34:45 +0300

    Remove deprecated pkg

--- linux-arch-x86_64-hyprland-glass/setup.sh
+++ linux-arch-x86_64-hyprland-glass/setup.sh
@@ -213,7 +213,7 @@ sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/dconf
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/easyeffects/
 info "Installing easyeffects plugins..."
 trace pacman -S --needed --noconfirm lsp-plugins lsp-plugins-ladspa calf libebur128 zam-plugins zita-convolver speex soundtouch rnnoise libsamplerate libsndfile libbs2b fftw speexdsp nlohmann-json onetbb
-sudo -u "$DELEVATED_USER" paru -S --needed --noconfirm mda-lv2-git libdeep_filter_ladspa-bin calf-ladspa
+sudo -u "$DELEVATED_USER" paru -S --needed --noconfirm mda-lv2-git libdeep_filter_ladspa-bin
 
 info "Installing wofi..."
 trace pacman -S --needed --noconfirm wofi
