b6e26504e42d5b36fffe39cd408dd3228e46359e
kseen <korenevdn@gmail.com>
2025-07-04 02:58:21 +0300

    Fix wallpaper copy command to use sudo for proper user permissions

--- linux-arch-x86_64-hyprland-glass/setup.sh
+++ linux-arch-x86_64-hyprland-glass/setup.sh
@@ -200,7 +200,7 @@ trace cp config/hypr/hyprpaper.conf /home/$DELEVATED_USER/.config/hypr/
 mkdir -p /home/$DELEVATED_USER/Pictures/Wallpapers
 chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/Pictures/Wallpapers/
 chmod 775 /home/$DELEVATED_USER/Pictures/Wallpapers/
-trace cp "wallpapers/avogado6 - 2024.06.jpg" /home/$DELEVATED_USER/Pictures/Wallpapers/
+trace "sudo -u $DELEVATED_USER cp \"wallpapers/avogado6 - 2024.06.jpg\" \"/home/$DELEVATED_USER/Pictures/Wallpapers/avogado6 - 2024.06.jpg\""
 
 info "Installing hyprpicker..."
 trace pacman -S --needed --noconfirm hyprpicker
