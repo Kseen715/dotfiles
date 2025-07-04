59562ab25e2c2688a14890a61e6c492e86f07175
kseen <korenevdn@gmail.com>
2025-07-04 03:38:10 +0300

    Moved SDDM theme

--- linux-arch-x86_64-hyprland-glass/setup.sh
+++ linux-arch-x86_64-hyprland-glass/setup.sh
@@ -184,9 +184,9 @@ trace pacman -S --needed --noconfirm sddm qt6-5compat qt6-declarative qt6-svg
 info "Installing sddm dotfiles..."
 trace mkdir -p /etc/sddm.conf.d
 trace cp config/sddm/hyprland.main.conf /etc/sddm.conf.d/sddm.conf
-info "Installing sddm theme..." # config/sddm/jakoolit-theme
-trace mkdir -p /usr/share/sddm/themes/jakoolit-theme
-trace cp -r config/sddm/jakoolit-theme /usr/share/sddm/themes
+info "Installing sddm theme..." # config/sddm/glass-theme
+trace mkdir -p /usr/share/sddm/themes/glass-theme
+trace cp -r config/sddm/glass-theme /usr/share/sddm/themes
 trace mkdir -p /etc/sddm.conf.d
 trace cp config/sddm/theme.conf.user /etc/sddm.conf.d/theme.conf.user
 info "Activating sddm..."
