5df2cf290c0b329f339d4ec9c2b506a23f41ae1d
kseen <korenevdn@gmail.com>
2025-07-04 03:02:02 +0300

    Add debloat script for Windows 11 and update setup script to invoke it

--- windows-11-x86_64/setup.ps1
+++ windows-11-x86_64/setup.ps1
@@ -7,3 +7,6 @@ function InvokeEcho {
 
 # Disable the window shake gesture
 InvokeEcho "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\microscripts\window-shake-gesture-disable.ps1`"' -Verb RunAs -Wait -PassThru | Out-Null"
+
+# Debloat Windows 11
+InvokeEcho "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File `"$PSScriptRoot\microscripts\raphire-win11debloat.ps1`"' -Verb RunAs -Wait -PassThru | Out-Null"
