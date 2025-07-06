c57a7c9f910531a8d986960313836b975e25dbcf
kseen <korenevdn@gmail.com>
2025-07-06 22:58:17 +0300

    Refactor git installation script to ensure git is installed before other packages

--- linux-arch-x86_64-hyprland-glass/modules/git.sh
+++ linux-arch-x86_64-hyprland-glass/modules/git.sh
@@ -1,5 +1,6 @@
 # Ensure git is installed
 if ! command -v git &>/dev/null; then
     info "Git not found. Installing git..."
-    trace pacman -S --needed --noconfirm git wget nano vim man
+    trace pacman -S --needed --noconfirm git 
 fi
+trace pacman -S --needed --noconfirm wget nano vim man
