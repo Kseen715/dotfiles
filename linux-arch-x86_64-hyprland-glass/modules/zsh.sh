1f5787a310145d6a4e1ea2623e5d03c0e6d2aec4
kseen <korenevdn@gmail.com>
2025-07-06 22:59:16 +0300

    Add zsh shell change command to installation script

--- linux-arch-x86_64-hyprland-glass/modules/zsh.sh
+++ linux-arch-x86_64-hyprland-glass/modules/zsh.sh
@@ -3,3 +3,5 @@ trace cd $DOTFILES_KSEEN715_REPO/zsh
 trace chmod +x ./install-run.sh
 sudo -u "$DELEVATED_USER" ./install-run.sh -y
 trace cd $SCRIPT_DIR
+# change shell to zsh for the user
+chsh -s $(which zsh) "$DELEVATED_USER"
