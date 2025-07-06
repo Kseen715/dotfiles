79b48ed877e02000a3ec505a338b6a3218aefb08
kseen <korenevdn@gmail.com>
2025-07-06 23:41:33 +0300

    Fix file path in zsh installation script for chmod command

--- linux-arch-x86_64-hyprland-glass/modules/zsh.sh
+++ linux-arch-x86_64-hyprland-glass/modules/zsh.sh
@@ -1,6 +1,6 @@
 info "Installing zsh, dependencies and dotfiles..."
 trace cd $DOTFILES_KSEEN715_REPO/zsh
-trace chmod +x ./install-run.sh
+trace chmod +x $DOTFILES_KSEEN715_REPO/zsh/install-run.sh
 sudo -u "$DELEVATED_USER" $DOTFILES_KSEEN715_REPO/zsh/install-run.sh -y
 trace cd $SCRIPT_DIR
 # change shell to zsh for the user
