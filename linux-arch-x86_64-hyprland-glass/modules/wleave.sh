d39884339d53b2b4add94c1a08898a85da921b25
kseen <korenevdn@gmail.com>
2025-07-09 00:51:25 +0300

    Add wleave configuration with buttons, styles, and installation scripts

--- linux-arch-x86_64-hyprland-glass/modules/wleave.sh
+++ linux-arch-x86_64-hyprland-glass/modules/wleave.sh
@@ -1,7 +1,13 @@
-info "Installing wlogout..."
+info "Installing wleave..."
 trace sudo -u "$DELEVATED_USER" $AUR_HELPER -S --needed --noconfirm wleave-git
 trace mkdir -p /home/$DELEVATED_USER/.config/wleave
 trace chmod 775 /home/$DELEVATED_USER/.config/wleave
 trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/wleave
-trace cp "$(dirname "$(realpath "$0")")/config/wlogout-wleave/layout" /home/$DELEVATED_USER/.config/wleave/layout
-trace cp "$(dirname "$(realpath "$0")")/config/wlogout-wleave/style.css" /home/$DELEVATED_USER/.config/wleave/style.css
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/layout" /home/$DELEVATED_USER/.config/wleave/layout
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/style.css" /home/$DELEVATED_USER/.config/wleave/style.css
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/lock.png" /home/$DELEVATED_USER/.config/wleave/lock.png
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/logout.png" /home/$DELEVATED_USER/.config/wleave/logout.png
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/pause.png" /home/$DELEVATED_USER/.config/wleave/pause.png
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/power.png" /home/$DELEVATED_USER/.config/wleave/power.png
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/restart.png" /home/$DELEVATED_USER/.config/wleave/restart.png
+trace cp "$(dirname "$(realpath "$0")")/config/wleave/sleep.png" /home/$DELEVATED_USER/.config/wleave/sleep.png
--- linux-arch-x86_64-hyprland-glass/modules/wlogout.sh
+++ linux-arch-x86_64-hyprland-glass/modules/wlogout.sh
@@ -3,3 +3,11 @@ trace sudo -u "$DELEVATED_USER" $AUR_HELPER -S --needed --noconfirm wlogout
 trace mkdir -p /home/$DELEVATED_USER/.config/wlogout
 trace chmod 775 /home/$DELEVATED_USER/.config/wlogout
 trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/wlogout
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/layout" /home/$DELEVATED_USER/.config/wlogout/layout
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/style.css" /home/$DELEVATED_USER/.config/wlogout/style.css
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/lock.png" /home/$DELEVATED_USER/.config/wlogout/lock.png
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/logout.png" /home/$DELEVATED_USER/.config/wlogout/logout.png
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/pause.png" /home/$DELEVATED_USER/.config/wlogout/pause.png
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/power.png" /home/$DELEVATED_USER/.config/wlogout/power.png
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/restart.png" /home/$DELEVATED_USER/.config/wlogout/restart.png
+trace cp "$(dirname "$(realpath "$0")")/config/wlogout/sleep.png" /home/$DELEVATED_USER/.config/wlogout/sleep.png
