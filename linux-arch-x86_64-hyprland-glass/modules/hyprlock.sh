2c0d53af6baaeeb9f5c8f9c3fc8d6d763eef3cd8
kseen <korenevdn@gmail.com>
2025-07-10 16:05:59 +0300

    Add hyprlock and hypridle configuration files with installation script updates

--- linux-arch-x86_64-hyprland-glass/modules/hyprlock.sh
+++ linux-arch-x86_64-hyprland-glass/modules/hyprlock.sh
@@ -1,2 +1,9 @@
 info "Installing hyprlock..."
 trace pacman -S --needed --noconfirm hypridle hyprlock
+trace mkdir -p /home/$DELEVATED_USER/.config/hypr
+trace chmod 775 /home/$DELEVATED_USER/.config/hypr
+trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/hypr
+trace cp "$(dirname "$(realpath "$0")")/config/hypr/hyprlock.conf" /home/$DELEVATED_USER/.config/hypr/
+trace chmod 644 /home/$DELEVATED_USER/.config/hypr/hyprlock.conf
+trace cp "$(dirname "$(realpath "$0")")/config/hypr/hypridle.conf" /home/$DELEVATED_USER/.config/hypr/
+trace chmod 644 /home/$DELEVATED_USER/.config/hypr/hypridle.conf
