ca617cb17522f953592ebdfb8aa666f8ed8c6a7a
kseen <korenevdn@gmail.com>
2025-07-11 17:51:01 +0300

    Remove dotfiles download process from setup script

--- os-rice/linux-arch-x86_64-hyprland-glass/setup.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/setup.sh
@@ -16,10 +16,7 @@ source "$(dirname "$(realpath "$0")")/src/detect-gpu.sh"
 source "$(dirname "$(realpath "$0")")/modules/pacman-multilib.sh"
 source "$(dirname "$(realpath "$0")")/modules/gpu-drivers.sh"
 
-info "Downloading dotfiles from Kseen715..."
-DOTFILES_KSEEN715_REPO="$TMP_FOLDER/dotfiles_Kseen715"
-trace rm -rf $DOTFILES_KSEEN715_REPO
-trace git clone https://github.com/Kseen715/dotfiles $DOTFILES_KSEEN715_REPO --depth 1
+source "$(dirname "$(realpath "$0")")/src/repo-dotfiles.sh"
 
 source "$(dirname "$(realpath "$0")")/modules/wayland.sh"
 
