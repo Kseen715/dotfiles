1ede8abdec9a48a10bcfa9b9b63fbf577f7655b3
Kseen715 <korenevdn@gmail.com>
2025-07-13 00:01:51 +0300

    Update audio startup script to set default sink and source for Easyeffects

--- os-rice/linux-arch-x86_64-hyprland-glass/config/hypr/start-audio.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/config/hypr/start-audio.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# pactl set-default-sink alsa_output.usb-Logitech_G733_Gaming_Headset-00.analog-stereo
 
 # launch qpwgraph with ~/.patchbay as the config file
 if [ -f ~/.patchbay ]; then
     qpwgraph -a -m ~/.patchbay &
+    pactl set-default-sink easyeffects_sink
+    pactl set-default-source easyeffects_source
 fi
