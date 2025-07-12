ec5f96e4d7dc0e0bd9159c680ae89b5fce5ea180
Kseen715 <korenevdn@gmail.com>
2025-07-12 23:24:08 +0300

    Add installation script for OnlyOffice

--- os-rice/linux-arch-x86_64-hyprland-glass/apps/vscode-insiders.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/apps/vscode-insiders.sh
@@ -1,2 +1,3 @@
 info "Installing VSCode Insiders..."
 trace sudo -u "$DELEVATED_USER" $AUR_HELPER -S --needed --noconfirm visual-studio-code-insiders-bin
+trace pacman -S --needed --noconfirm ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-iosevkaterm-nerd
