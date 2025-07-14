e5043d0d5ef651cbd44cf32666b8a9e612fdedae
Kseen715 <korenevdn@gmail.com>
2025-07-15 02:01:35 +0300

    Update Telegram installation script to include webkit2gtk dependency

--- os-rice/linux-arch-x86_64-hyprland-glass/apps/telegram.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/apps/telegram.sh
@@ -1,5 +1,5 @@
 info "Installing Telegram..."
-trace pacman -S --needed --noconfirm telegram-desktop
+trace pacman -S --needed --noconfirm telegram-desktop webkit2gtk-4.1
 trace mkdir -p /home/$DELEVATED_USER/.local/share/TelegramDesktop
 trace chmod 775 /home/$DELEVATED_USER/.local/share/TelegramDesktop
 trace chown -R $DELEVATED_USER:$DELEVATED_USER /home/$DELEVATED_USER/.local/share/TelegramDesktop
