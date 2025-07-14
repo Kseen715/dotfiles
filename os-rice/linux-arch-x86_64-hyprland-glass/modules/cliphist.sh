49b8ff71ba1ae31677d88d82be060d8908c74377
Kseen715 <korenevdn@gmail.com>
2025-07-14 20:23:04 +0300

    Update cliphist installation script; streamline Golang check and add image download

--- os-rice/linux-arch-x86_64-hyprland-glass/modules/cliphist.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/modules/cliphist.sh
@@ -1,2 +1,17 @@
 info "Installing cliphist..."
 trace pacman -S --needed --noconfirm cliphist
+
+trace cp $SCRIPT_DIR/config/hypr/start-cliphist-store.sh /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
+trace chmod +x /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
+trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
+info "Checking if Golang installed..."
+if ! command -v go &>/dev/null; then
+    info "Golang not found. Installing Golang..."
+    trace pacman -S --needed --noconfirm go
+else
+    info "Golang is installed"
+fi
+trace sudo -u $DELEVATED_USER go install github.com/pdf/cliphist-wofi-img@latest
+trace wget https://raw.githubusercontent.com/sentriz/cliphist/refs/heads/master/contrib/cliphist-wofi-img -O /usr/local/bin/cliphist-wofi-img
+trace chmod +x /usr/local/bin/cliphist-wofi-img
+trace chown "$DELEVATED_USER":"$DELEVATED_USER" /usr/local/bin/cliphist-wofi-img
--- os-rice/linux-arch-x86_64-hyprland-glass/modules/hyprland.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/modules/hyprland.sh
@@ -24,22 +24,6 @@ trace cp $SCRIPT_DIR/config/hypr/start-easyeffects.sh /home/$DELEVATED_USER/.con
 trace chmod +x /home/$DELEVATED_USER/.config/hypr/start-easyeffects.sh
 trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/hypr/start-easyeffects.sh
 
-# Start of cliphist
-trace cp $SCRIPT_DIR/config/hypr/start-cliphist-store.sh /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
-trace chmod +x /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
-trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/hypr/start-cliphist-store.sh
-info "Checking if Golang installed..."
-if ! command -v go &>/dev/null; then
-    info "Golang not found. Installing Golang..."
-    trace pacman -S --needed --noconfirm go
-else
-    info "Golang is installed"
-fi
-trace go install github.com/pdf/cliphist-wofi-img@latest
-trace wget https://raw.githubusercontent.com/sentriz/cliphist/refs/heads/master/contrib/cliphist-wofi-img -O /usr/local/bin/cliphist-wofi-img
-trace chmod +x /usr/local/bin/cliphist-wofi-img
-trace chown "$DELEVATED_USER":"$DELEVATED_USER" /usr/local/bin/cliphist-wofi-img
-
 # Start of top
 trace cp $SCRIPT_DIR/config/hypr/start-top.sh /home/$DELEVATED_USER/.config/hypr/start-top.sh
 trace chmod +x /home/$DELEVATED_USER/.config/hypr/start-top.sh
