c61fdf5a0da2573853dbcdf4085d2fb61516311c
Kseen715 <korenevdn@gmail.com>
2025-07-19 19:50:13 +0300

    Enhance printer setup script to install additional packages and configure Samba

--- os-rice/linux-arch-x86_64-hyprland-glass/modules/printer.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/modules/printer.sh
@@ -1,2 +1,8 @@
 info "Installing smbclient..."
-trace pacman -S --needed --noconfirm smbclient
+trace pacman -S --needed --noconfirm smbclient cups samba
+trace sudo -u "$DELEVATED_USER" $AUR_HELPER -S --needed --noconfirm captdriver-git
+trace mkdir -p /etc/samba
+trace touch /etc/samba/smb.conf
+trace chmod 644 /etc/samba/smb.conf
+trace systemctl enable --now smb.service
+trace systemctl enable --now cups.service
