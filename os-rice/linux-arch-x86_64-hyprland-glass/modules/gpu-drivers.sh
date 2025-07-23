e0d1f7ab75af111f39c1a78c810ef8a4343e135f
Kseen715 <korenevdn@gmail.com>
2025-07-23 23:17:38 +0300

    Updated NVIDIA GPU installer

--- os-rice/linux-arch-x86_64-hyprland-glass/modules/gpu-drivers.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/modules/gpu-drivers.sh
@@ -6,7 +6,39 @@ for vendor in "${vendors[@]}"; do
     case "$vendor" in
         NVIDIA)
             info "NVIDIA GPU detected"
-            trace sudo pacman -S --needed --noconfirm nvidia-open vulkan-icd-loader lib32-vulkan-icd-loader vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers nvidia-settings nvidia-utils lib32-nvidia-utils nvidia-prime
+            # check if nvidia-open-dkms is in ignore list
+            to_install = ""
+            if grep -q "nvidia-open-dkms" /etc/pacman.conf; then
+                warning "nvidia-open-dkms is in the ignore list, skipping installation"
+            else
+                to_install="nvidia-open-dkms"
+            fi
+            if grep -q "nvidia-settings" /etc/pacman.conf; then
+                warning "nvidia-settings is in the ignore list, skipping installation"
+            else
+                to_install="$to_install nvidia-settings"
+            fi
+            if grep -q "nvidia-utils" /etc/pacman.conf; then
+                warning "nvidia-utils is in the ignore list, skipping installation"
+            else
+                to_install="$to_install nvidia-utils"
+            fi
+            if grep -q "lib32-nvidia-utils" /etc/pacman.conf; then
+                warning "lib32-nvidia-utils is in the ignore list, skipping installation"
+            else
+                to_install="$to_install lib32-nvidia-utils"
+            fi
+            if [ -z "$to_install" ]; then
+                warning "No NVIDIA packages to install, skipping"
+            else
+                info "Installing NVIDIA packages: $to_install"
+                trace sudo pacman -S --needed --noconfirm $to_install
+            fi
+            trace sudo pacman -S --needed --noconfirm vulkan-icd-loader lib32-vulkan-icd-loader vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers nvidia-prime
+            trace pacman -Qi nvidia-open-dkms | grep Version | awk '{print $3}' | sed 's/-2//g' > /tmp/nvidia_version
+            nvidia_version=$(cat /tmp/nvidia_version)
+            info "NVIDIA version: $nvidia_version"
+            trace dkms install --no-depmod nvidia/$nvidia_version
             ;;
         AMD)
             info "AMD GPU detected"
