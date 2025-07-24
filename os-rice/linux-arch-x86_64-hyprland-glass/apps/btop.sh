02231e33721d4e055e0ddf5999dfaeab6194c954
Kseen715 <korenevdn@gmail.com>
2025-07-24 23:47:33 +0300

    Add configuration files for btop and foot, and update installation scripts

--- os-rice/linux-arch-x86_64-hyprland-glass/apps/btop.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/apps/btop.sh
@@ -4,6 +4,6 @@ info "Installing btop dotfiles..."
 trace sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/btop
 trace chmod 775 /home/$DELEVATED_USER/.config/btop
 trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/btop
-trace cp $SCRIPT_DIR/config/btop/btop.conf /home/$DELEVATED_USER/.config/btop/btop.conf
+trace cp $DOTFILES_KSEEN715_REPO/btop/btop.conf /home/$DELEVATED_USER/.config/btop/btop.conf
 trace chmod 644 /home/$DELEVATED_USER/.config/btop/btop.conf
 trace chown "$DELEVATED_USER":"$DELEVATED_USER" /home/$DELEVATED_USER/.config/btop/btop.conf
--- os-rice/linux-arch-x86_64-hyprland-glass/modules/foot.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/modules/foot.sh
@@ -2,4 +2,5 @@ info "Installing foot..."
 trace pacman -S --needed --noconfirm foot
 info "Installing foot dotfiles..."
 sudo -u "$DELEVATED_USER" mkdir -p /home/$DELEVATED_USER/.config/foot
-trace cp $SCRIPT_DIR/config/foot/foot.ini /home/$DELEVATED_USER/.config/foot/
+trace cp $SCRIPT_DIR/config/foot/foot-colors.ini /home/$DELEVATED_USER/.config/foot/
+trace cp $DOTFILES_KSEEN715_REPO/foot/foot.ini /home/$DELEVATED_USER/.config/foot/
