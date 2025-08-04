c342865357420f06f708f2ffbed41d828290362e
Kseen715 <korenevdn@gmail.com>
2025-08-05 01:48:54 +0300

    Enhance dkms installation script with kernel header installation messages

--- os-rice/linux-arch-x86_64-hyprland-glass/modules/dkms.sh
+++ os-rice/linux-arch-x86_64-hyprland-glass/modules/dkms.sh
@@ -1,12 +1,16 @@
 info "Installing dkms..."
 trace pacman -S --needed --noconfirm dkms
 # if 'linux' pkg is installed
-if pacman -Qq | grep -qw linux; then
+info "Checking for installed Linux kernel..."
+if trace "pacman -Qq | grep -qw linux"; then
+    info "Installing linux headers for dkms..."
     trace pacman -S --needed --noconfirm linux-headers
 fi
-if pacman -Qq | grep -qw linux-lts; then
+if trace "pacman -Qq | grep -qw linux-lts"; then
+    info "Installing linux-lts headers for dkms..."
     trace pacman -S --needed --noconfirm linux-lts-headers
 fi
-if pacman -Qq | grep -qw linux-zen; then
+if trace "pacman -Qq | grep -qw linux-zen"; then
+    info "Installing linux-zen headers for dkms..."
     trace pacman -S --needed --noconfirm linux-zen-headers
 fi
