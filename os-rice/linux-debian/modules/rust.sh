c4bda895fcd9e4c12af93cfe63fcb737151664cb
Kseen715 <korenevdn@gmail.com>
2025-09-12 23:57:32 +0300

    Updated Rust installer

--- os-rice/linux-debian/modules/rust.sh
+++ os-rice/linux-debian/modules/rust.sh
@@ -1,5 +1,10 @@
-info "Installing Rust via rustup..."
+info "Installing Rust..."
 install_pkg_apt curl
+if [[ ":$PATH:" != *":/home/$DELEVATED_USER/.cargo/bin:"* ]]; then
+    trace "Adding /home/$DELEVATED_USER/.cargo/bin to PATH"
+    PATH="$PATH:/home/$DELEVATED_USER/.cargo/bin"
+    export PATH
+fi
 source /home/$DELEVATED_USER/.cargo/env
 trace cargo --version
 if cargo --version &> /dev/null; then
@@ -8,7 +13,9 @@ if cargo --version &> /dev/null; then
     trace "sudo -u $DELEVATED_USER rustup update"
 else
     trace "sudo -u $DELEVATED_USER curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo -u $DELEVATED_USER sh -s -- -y --default-toolchain stable"
-    source /home/$DELEVATED_USER/.cargo/env
+    trace source /home/$DELEVATED_USER/.cargo/env
     trace . "/home/$DELEVATED_USER/.cargo/env"
+    trace sudo -u $DELEVATED_USER rustup default stable
+    # trace rustup update
     trace cargo --version
 fi
